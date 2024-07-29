2fefc592a0b366f789689ffdfd482fa411c137d6
Rasel Kibria <rasel5jkibria@gmail.com>
2024-07-30 00:43:35 +0600

    User Entity, DbContext, IRepository, Repository Created

---
+++ CafeBillingSystem/CafeBillingSystem/DataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBillingSystem.DataAccessLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
